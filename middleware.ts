a307cd550efa54687434c0f0f9f40a8881e19ff5
Mariano <mateandtalk@gmail.com>
2026-06-16 16:15:53 -0300

    fix: update middleware to new clerk syntax

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,13 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs';
 
-export default clerkMiddleware({
-  publicRoutes: ['/success'],
+// Definimos qué rutas son públicas
+const isPublicRoute = createRouteMatcher(['/success', '/']);
+
+export default clerkMiddleware((auth, req) => {
+  if (isPublicRoute(req)) return; // Si es pública, no hacemos nada
+  auth().protect(); // Si no es pública, protegemos la ruta
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
